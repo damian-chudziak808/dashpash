5f106a5919f0833d9d1ed2c393685d8f4ba29f3a
Damian Chudziak <damian.chudziak808@gmail.com>
2023-02-27 17:40:04 +0100

    404 page

--- client/src/pages/NotFound/NotFound.tsx
+++ client/src/pages/NotFound/NotFound.tsx
@@ -1,6 +1,26 @@
+import { useNavigate } from 'react-router-dom'
+import { RiQuillPenLine } from 'react-icons/ri'
+
 const NotFound = () => {
+  const navigate = useNavigate()
   return (
-    <div>NotFound</div>
+    <div className="flex min-h-screen flex-col items-center justify-center">
+      <div className="flex flex-col justify-center">
+        <h1 className="mb-2 text-center font-serif text-4xl font-bold">Not found</h1>
+        <p className="mx-auto text-gray-600">
+          The page you are looking for does not exist... yet
+        </p>
+        <div className="mx-auto mt-8 flex gap-4">
+          <button className="btn btn--dark" onClick={() => navigate(-1)}>
+            Go back
+          </button>
+          <a href="mailto:dashpash@gmail.com" className="btn btn--outline gap-2">
+            <RiQuillPenLine className="h-4 w-4" />
+            Report an issue
+          </a>
+        </div>
+      </div>
+    </div>
   )
 }
 
